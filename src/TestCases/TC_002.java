160e5f76f9ac47e09d2c3e8ba027ff0e237c8d26
Sairambanda8 <sairambanda8@gmail.com>
2019-07-27 02:54:45 -0400

    sairam learn

---
+++ src/TestCases/TC_002.java
@@ -0,0 +1,39 @@
+package TestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class TC_002 {
+	
+	
+		@Test
+		public void setup2() {
+			System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+			 
+
+			WebDriver driver=new ChromeDriver();
+			 
+
+			driver.get("https://ocs.ca/");
+			 
+			// Maximize browser
+			 
+			driver.manage().window().maximize();
+			Select dropdown_day = new Select(driver.findElement(By.id("dob__day")));
+			dropdown_day.selectByIndex(1);
+			Select dropdown_month = new Select(driver.findElement(By.id("dob__month")));
+			dropdown_month.selectByIndex(3);
+			Select dropdown_year = new Select(driver.findElement(By.id("dob__year")));
+			dropdown_year.selectByValue("1950");
+			driver.findElement(By.cssSelector("button.btn.btn--outline")).click();;
+			driver.findElement(By.cssSelector("span.checkbox")).click();
+			driver.findElement(By.cssSelector("button.btn.btn--outline")).click();; 
+			driver.quit();
+
+			 
+			}
+
+}
